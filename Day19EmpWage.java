e778d8ae163087753a190727ca1440cc0bc07e64
ravi9038 <ravijadhav9038@gmail.com>
2020-07-15 18:39:45 +0530

    Calculate wage For Month

--- Day19EmpWage.java
+++ Day19EmpWage.java
@@ -1,19 +1,21 @@
 
 public class Day19EmpWage{
 
-public static final int isPartTime = 1;
+	public static final int isPartTime = 1;
 	public static final int isFullTime = 2;
 	public static final int empRatePerHr=20;
 	public static final int numOfWorkingDays=3;
+	public static final int maxHrsInMonth=10;
 
 	public static void main(String [] args){
 
 
 		int empHrs = 0;
-		int empWage = 0;
-		int totalEmpWage = 0;
+		int totalEmpHrs = 0;
+		int totalWorkingDays = 0;
 
-		for(int day = 0; day < numOfWorkingDays; day++){
+		while (totalEmpHrs <= maxHrsInMonth && totalWorkingDays < numOfWorkingDays){
+			totalWorkingDays++;
 
 			int empCheck = (int) Math.floor(Math.random()*10) % 3;
 
@@ -29,10 +31,11 @@ public static final int isPartTime = 1;
 				default :
 					empHrs = 0;
 			}
-			empWage = empHrs * empRatePerHr;
-			totalEmpWage += empWage;
-			System.out.println("Emp Wage : "+ empWage);
+			totalEmpHrs += empHrs;
+			System.out.println("Day : "+totalWorkingDays + " Emp Hrs : "+empHrs);
 		}
+
+		int totalEmpWage = totalEmpHrs * empRatePerHr;
 		System.out.println("Total Emp Wage "+ totalEmpWage);
 	}
 }
